e9930648daa1fffefa3c835c354a8c6c391e88ad
heyitsmeravi <ravijnv2428@gmail.com>
2026-03-03 15:11:53 +0530

    3rd march 2026 more questions

---
+++ recur_arrayPrint.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+        void printArray(vector<int>arr,int idx){
+            if (idx==arr.size()){
+                return;
+            }
+            cout<<arr[idx]<<endl;
+            printArray(arr,idx+1);
+        }
+};
+int main(){
+    Solution obj;
+    obj.printArray({1,2,3,4},0);
+    return 0;
+}
---
+++ recur_arrayReversePrint.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+    void arrayReversePrint(vector<int>&arr,int idx){
+        if (idx==arr.size()){
+            return ;
+        }
+        arrayReversePrint(arr,idx+1);
+        cout<<arr[idx]<<endl;
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int>arr={1,2,3,4};
+    int idx=0;
+    obj.arrayReversePrint(arr,idx);
+    return 0;
+}
